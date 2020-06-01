ed6081b3d87ab48d70e0b11bb2a9cb33ce4200d7
M042 <boukobza.nathan@gmail.com>
2020-06-01 19:36:52 +0200

    remove useless condition

--- purebeurre/views/products.py
+++ purebeurre/views/products.py
@@ -203,18 +203,15 @@ class FindSubstituteView(ListView):
             a function to redirect to a specific url
         """
         if self.request.method == "POST":
-            if self.request.user.is_authenticated:
-                sub = self.request.POST['substitute']
-                substitute = Product.objects.get(pk=sub)
-                user = self.request.user
-                try:
-                    Favorite.objects.get(user=user, substitute=substitute)
-                except ObjectDoesNotExist:
-                    Favorite.objects.create(user=user, substitute=substitute)
+            sub = self.request.POST['substitute']
+            substitute = Product.objects.get(pk=sub)
+            user = self.request.user
+            try:
+                Favorite.objects.get(user=user, substitute=substitute)
+            except ObjectDoesNotExist:
+                Favorite.objects.create(user=user, substitute=substitute)
 
-                return redirect('/')
-            else:
-                return redirect('/login/')
+            return redirect('/')
 
 
 class FavoriteView(LoginRequiredMixin, ListView):
